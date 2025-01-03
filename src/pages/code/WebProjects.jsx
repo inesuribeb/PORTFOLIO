116ed5514a19b32aadddd11eda92a895bdfcc426
inesuribeb <inesuribeb@gmail.com>
2025-01-03 17:27:18 +0100

    web projects

--- src/pages/code/WebProjects.jsx
+++ src/pages/code/WebProjects.jsx
@@ -5,15 +5,15 @@ import './WebProjects.css'
 
 function WebProjects() {
   const [activeIndex, setActiveIndex] = useState(0);
-const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const sections = document.querySelectorAll('.web-section');
       const position = window.pageYOffset;
-      
+
       setScrollPosition(position);
-      
+
       sections.forEach((section, index) => {
         const rect = section.getBoundingClientRect();
         if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
@@ -21,7 +21,7 @@ const [scrollPosition, setScrollPosition] = useState(0);
         }
       });
     };
-  
+
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
@@ -68,23 +68,50 @@ const [scrollPosition, setScrollPosition] = useState(0);
       { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
       { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },]
     },
-
+    {
+      title: "WWW MAGAZINE",
+      description: "WWW is an online magazine with impersonal content without any path.",
+      method: "Magazine • Editorial design • Web design",
+      link: "www.wwwmagazine.com",
+      images: [{ src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción1" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },]
+    },
+    {
+      title: "WWW MAGAZINE",
+      description: "WWW is an online magazine with impersonal content without any path.",
+      method: "Magazine • Editorial design • Web design",
+      link: "www.wwwmagazine.com",
+      images: [{ src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción1" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },
+      { src: "https://www.nationalflags.shop/WebRoot/vilkasfi01/Shops/2014080403/53F0/FD66/F92E/79C5/D07A/0A28/100B/4C14/rectangle_yellow_flag.jpg", alt: "descripción2" },]
+    },
   ];
 
   return (
 
     // <div className="web-projects">
     // <div className={`web-projects ${activeIndex > 0 ? 'scrolling' : ''}`}>
-          <div className="web-projects" style={{ marginBottom: `${marginBottom}px` }}>
+    <div className="webproject-container">
+      <div className="web-projects" style={{ marginBottom: `${marginBottom}px` }}>
 
 
-      {projectsData.map((project, index) => (
-        <SectionWeb
-          key={index}
-          className={index === activeIndex ? 'active' : ''}
-          {...project}
-        />
-      ))}
+        {projectsData.map((project, index) => (
+          <SectionWeb
+            key={index}
+            className={index === activeIndex ? 'active' : ''}
+            {...project}
+          />
+        ))}
+      </div>
     </div>
   );
 }
