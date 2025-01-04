95108828642855290b37040d442577b388db7ee4
inesuribeb <inesuribeb@gmail.com>
2025-01-04 16:43:37 +0100

    modal styled

--- src/pages/art/Modal.jsx
+++ src/pages/art/Modal.jsx
@@ -1,18 +1,20 @@
 import './Modal.css'
 
 function Modal({ isOpen, onClose, image, onNext, onPrevious }) {
-    if (!isOpen) return null;
-  
-    return (
-      <div className="modal-overlay" onClick={onClose}>
-        <div className="modal-content" onClick={e => e.stopPropagation()}>
-          <button className="close-button" onClick={onClose}>×</button>
-          <button className="nav-button prev" onClick={onPrevious}>‹</button>
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
+        <button className="close-button" onClick={onClose}>×</button>
+        <button className="nav-button prev" onClick={onPrevious}>‹</button>
+        <div className="image-wrapper">  {/* Agregamos el wrapper aquí */}
           <img src={image.src} alt={image.alt} />
-          <button className="nav-button next" onClick={onNext}>›</button>
         </div>
+        <button className="nav-button next" onClick={onNext}>›</button>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Modal;
