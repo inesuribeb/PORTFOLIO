786989fb485f842c827fd6432ea80712b2dfb9b7
inesuribeb <inesuribeb@gmail.com>
2025-01-06 12:36:26 +0100

    filtered categories for web projects, frontend, all or full stack

--- src/pages/code/WebProjects.jsx
+++ src/pages/code/WebProjects.jsx
@@ -6,6 +6,8 @@ import './WebProjects.css'
 function WebProjects() {
   const [activeIndex, setActiveIndex] = useState(0);
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,97 +34,127 @@ function WebProjects() {
     {
       title: "SWIPE AGENCY",
       description: "SWIPE AGENCY is a fashion representation agency based in Bilbao. Its brands combine design and tradition.",
+      category: "Front-end",
       method: "React • JavaScript • CSS3",
       link: "https://github.com/inesuribeb/Swipe-Agency",
       images: [
-      { src: "https://i.ibb.co/2ZN5PWg/Captura-de-Pantalla-2025-01-05-a-las-14-37-55.png", alt: "descripción1" },
-      { src: "https://i.ibb.co/f297Rbr/IMG-6464.jpg", alt: "descripción2" },
-      { src: "https://i.ibb.co/wYrqG72/Captura-de-Pantalla-2025-01-05-a-las-14-38-04.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/wLJ4tHm/IMG-6463.jpg", alt: "descripción2" },
-      { src: "https://i.ibb.co/FYD5818/Captura-de-Pantalla-2025-01-05-a-las-14-38-13.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/34fpbXf/IMG-6456.jpg", alt: "descripción2" },
-      { src: "https://i.ibb.co/HnbQfMF/Captura-de-Pantalla-2025-01-05-a-las-14-38-53.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/YLxXVG1/IMG-6458.jpg", alt: "descripción2" },
-      { src: "https://i.ibb.co/B6YTXBy/Captura-de-Pantalla-2025-01-05-a-las-14-39-26.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/wCxz7kr/IMG-6457.jpg", alt: "descripción2" },
-      { src: "https://i.ibb.co/y4PbtNS/Captura-de-Pantalla-2025-01-05-a-las-14-39-04.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/0hKVKy7/IMG-6459.jpg", alt: "descripción2" },
-      { src: "https://i.ibb.co/WgKJpSx/IMG-6460.jpg", alt: "descripción2" },
-    ]
+        { src: "https://i.ibb.co/2ZN5PWg/Captura-de-Pantalla-2025-01-05-a-las-14-37-55.png", alt: "descripción1" },
+        { src: "https://i.ibb.co/f297Rbr/IMG-6464.jpg", alt: "descripción2" },
+        { src: "https://i.ibb.co/wYrqG72/Captura-de-Pantalla-2025-01-05-a-las-14-38-04.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/wLJ4tHm/IMG-6463.jpg", alt: "descripción2" },
+        { src: "https://i.ibb.co/FYD5818/Captura-de-Pantalla-2025-01-05-a-las-14-38-13.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/34fpbXf/IMG-6456.jpg", alt: "descripción2" },
+        { src: "https://i.ibb.co/HnbQfMF/Captura-de-Pantalla-2025-01-05-a-las-14-38-53.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/YLxXVG1/IMG-6458.jpg", alt: "descripción2" },
+        { src: "https://i.ibb.co/B6YTXBy/Captura-de-Pantalla-2025-01-05-a-las-14-39-26.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/wCxz7kr/IMG-6457.jpg", alt: "descripción2" },
+        { src: "https://i.ibb.co/y4PbtNS/Captura-de-Pantalla-2025-01-05-a-las-14-39-04.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/0hKVKy7/IMG-6459.jpg", alt: "descripción2" },
+        { src: "https://i.ibb.co/WgKJpSx/IMG-6460.jpg", alt: "descripción2" },
+      ]
     },
     {
       title: "MACHIMBAG",
       web: "https://machimba.inesuribe.es/home",
       description: "MACHIMBA is an online store offering unique, limited-edition bags designed and crafted by Rocío Machimbarrena. Developed in collaboration with Samuel Correia and Ekaitz Guerra.",
+      category: "Full-stack",
       method: "Node.js • MySQL • JavaScript • Pug • CSS3",
       link: "https://github.com/inesuribeb/Machimba",
       images: [
-      { src: "https://i.ibb.co/BP1v1BX/Captura-de-Pantalla-2025-01-05-a-las-10-44-45.png", alt: "descripción1" },
-      { src: "https://i.ibb.co/dcWB4nJ/Captura-de-Pantalla-2025-01-05-a-las-10-45-23.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/vZ4W5YX/WEB-ABOUT-DIOSA.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/dGzsKvK/Captura-de-Pantalla-2025-01-05-a-las-10-50-18.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/x7yz2MH/Captura-de-Pantalla-2025-01-05-a-las-10-48-48.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/M9J7B6T/Captura-de-Pantalla-2025-01-05-a-las-13-20-12.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/MSRCwG1/Captura-de-Pantalla-2025-01-05-a-las-10-51-16.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/CvY7hck/Captura-de-Pantalla-2025-01-05-a-las-13-26-42.png", alt: "descripción2" },
-    ]
+        { src: "https://i.ibb.co/BP1v1BX/Captura-de-Pantalla-2025-01-05-a-las-10-44-45.png", alt: "descripción1" },
+        { src: "https://i.ibb.co/dcWB4nJ/Captura-de-Pantalla-2025-01-05-a-las-10-45-23.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/vZ4W5YX/WEB-ABOUT-DIOSA.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/dGzsKvK/Captura-de-Pantalla-2025-01-05-a-las-10-50-18.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/x7yz2MH/Captura-de-Pantalla-2025-01-05-a-las-10-48-48.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/M9J7B6T/Captura-de-Pantalla-2025-01-05-a-las-13-20-12.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/MSRCwG1/Captura-de-Pantalla-2025-01-05-a-las-10-51-16.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/CvY7hck/Captura-de-Pantalla-2025-01-05-a-las-13-26-42.png", alt: "descripción2" },
+      ]
     },
     {
       title: "The Bike Country",
       description: "TBC is a premium tourism company specialized in cycling in the Basque Country (Euskadi). Developed in collaboration with Yeray Moreno and Estefania Mendes.",
+      category: "Full-stack",
       method: "MySQL • React • CSS3 • JavaScript",
       link: "https://github.com/inesuribeb/The-Bike_Country-Frontend-2-",
       images: [
-      { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
-      { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
-      { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
-      { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
-      { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
-      { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
-    ]
+        { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
+        { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
+        { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
+        { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
+        { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
+        { src: "https://cdn.pixabay.com/photo/2023/03/16/15/17/rectangle-7856945_1280.png", alt: "descripción1" },
+      ]
     },
     {
       title: "KHROMA",
       web: "https://khroma.inesuribe.es/",
       description: "Khroma is a project that allows users to explore and generate custom color schemes.",
+      category: "Front-end",
       method: "JavaScript • API REST • CSS3",
       link: "https://github.com/inesuribeb/Khroma",
       images: [
-      { src: "https://i.ibb.co/RSYwyM7/Captura-de-Pantalla-2025-01-05-a-las-11-06-58.png", alt: "descripción1" },
-      // { src: "https://i.ibb.co/kmC40Ng/Captura-de-Pantalla-2025-01-05-a-las-11-04-03.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/B2s3VkS/Captura-de-Pantalla-2025-01-05-a-las-11-08-21.png", alt: "descripción2" },   
-      { src: "https://i.ibb.co/4tL0LJD/Captura-de-Pantalla-2025-01-05-a-las-11-14-22.png", alt: "descripción2" },
-      // { src: "https://i.ibb.co/0ZrjwD3/Captura-de-Pantalla-2025-01-05-a-las-11-13-29.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/LrXqhMw/Captura-de-Pantalla-2025-01-05-a-las-11-15-42.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/wKMp1W6/Captura-de-Pantalla-2025-01-05-a-las-11-14-48.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/FWd9xzw/Captura-de-Pantalla-2025-01-05-a-las-16-55-00.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/bgwkgMz/Captura-de-Pantalla-2025-01-05-a-las-16-55-41.png", alt: "descripción2" },
-    ]
+        { src: "https://i.ibb.co/RSYwyM7/Captura-de-Pantalla-2025-01-05-a-las-11-06-58.png", alt: "descripción1" },
+        // { src: "https://i.ibb.co/kmC40Ng/Captura-de-Pantalla-2025-01-05-a-las-11-04-03.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/B2s3VkS/Captura-de-Pantalla-2025-01-05-a-las-11-08-21.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/4tL0LJD/Captura-de-Pantalla-2025-01-05-a-las-11-14-22.png", alt: "descripción2" },
+        // { src: "https://i.ibb.co/0ZrjwD3/Captura-de-Pantalla-2025-01-05-a-las-11-13-29.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/LrXqhMw/Captura-de-Pantalla-2025-01-05-a-las-11-15-42.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/wKMp1W6/Captura-de-Pantalla-2025-01-05-a-las-11-14-48.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/FWd9xzw/Captura-de-Pantalla-2025-01-05-a-las-16-55-00.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/bgwkgMz/Captura-de-Pantalla-2025-01-05-a-las-16-55-41.png", alt: "descripción2" },
+      ]
     },
     {
       title: "BYTE BITES",
+      web: "https://byte-bites.inesuribe.es/",
       description: "Byte Bites is a web platform designed to make exploring and discovering recipes easier. Developed in collaboration with Jon Latxiondo.",
+      category: "Front-end",
       method: "JavaScript • API REST • CSS3",
       link: "https://github.com/inesuribeb/Byte-Bites",
       images: [
-      { src: "https://i.ibb.co/NSm28g6/Captura-de-Pantalla-2025-01-05-a-las-10-31-02.png", alt: "descripción1" },
-      { src: "https://i.ibb.co/h2fHPgc/Captura-de-Pantalla-2025-01-05-a-las-10-31-47.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/bmFwMcJ/Captura-de-Pantalla-2025-01-05-a-las-10-39-57.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/P9fXLxp/Captura-de-Pantalla-2025-01-05-a-las-10-33-56.png", alt: "descripción2" },
-      { src: "https://i.ibb.co/nnZZdQh/Captura-de-Pantalla-2025-01-05-a-las-10-40-04.png", alt: "descripción2" },
-    ]
+        { src: "https://i.ibb.co/NSm28g6/Captura-de-Pantalla-2025-01-05-a-las-10-31-02.png", alt: "descripción1" },
+        { src: "https://i.ibb.co/h2fHPgc/Captura-de-Pantalla-2025-01-05-a-las-10-31-47.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/bmFwMcJ/Captura-de-Pantalla-2025-01-05-a-las-10-39-57.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/P9fXLxp/Captura-de-Pantalla-2025-01-05-a-las-10-33-56.png", alt: "descripción2" },
+        { src: "https://i.ibb.co/nnZZdQh/Captura-de-Pantalla-2025-01-05-a-las-10-40-04.png", alt: "descripción2" },
+      ]
     },
   ];
 
+  const filteredProjects = selectedCategory === 'All'
+    ? projectsData
+    : projectsData.filter(project => project.category === selectedCategory);
+
   return (
-
-    // <div className="web-projects">
-    // <div className={`web-projects ${activeIndex > 0 ? 'scrolling' : ''}`}>
     <div className="webproject-container">
+      <div className="categories">
+        <span
+          className={selectedCategory === 'All' ? 'active' : ''}
+          onClick={() => setSelectedCategory('All')}
+        >
+          All
+        </span>
+        <span className="separator">, </span>
+        <span
+          className={selectedCategory === 'Front-end' ? 'active' : ''}
+          onClick={() => setSelectedCategory('Front-end')}
+        >
+          Front-end
+        </span>
+        <span className="separator">, </span>
+        <span
+          className={selectedCategory === 'Full-stack' ? 'active' : ''}
+          onClick={() => setSelectedCategory('Full-stack')}
+        >
+          Full-stack
+        </span>
+      </div>
+
+
+
       <div className="web-projects" style={{ marginBottom: `${marginBottom}px` }}>
-
-
-        {projectsData.map((project, index) => (
+        {filteredProjects.map((project, index) => (
           <SectionWeb
             key={index}
             className={index === activeIndex ? 'active' : ''}
